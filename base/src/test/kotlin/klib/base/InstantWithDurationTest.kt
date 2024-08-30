92a5727a01f5bd1009ee420b90a211a4a17fc09d
Elifarley C. <519940+elifarley@users.noreply.github.com>
2024-08-30 17:46:24 +0000

    Use 26 bits for BITS_FOR_DURATION

--- base/src/test/kotlin/klib/base/InstantWithDurationTest.kt
+++ base/src/test/kotlin/klib/base/InstantWithDurationTest.kt
@@ -12,36 +12,35 @@ class InstantWithDurationTest {
     @Test
     fun `constructor with startEpochSeconds and durationMinutes creates correct packedValue`() {
         val instantWithDuration = InstantWithDuration
-            .fromStartAndDuration(EPOCH_2020, InstantWithDuration.MAX_DURATION_MINUTES)
+            .fromStartAndDuration(EPOCH_2020, MAX_DURATION_MINUTES)
         assertEquals("2020-01-01T00:00:00Z", instantWithDuration.startFormatted, "startFormatted")
-        assertEquals("2020-01-02T10:07:00Z", instantWithDuration.endFormatted, "endFormatted")
+        assertEquals("2147-08-06T09:03:00Z", instantWithDuration.endFormatted, "endFormatted")
         assertEquals(
             60 * DURATION_MINUTES_MASK,
             instantWithDuration.endEpochSeconds - instantWithDuration.startEpochSeconds,
             "Max duration"
         )
         assertEquals(DURATION_MINUTES_MASK, instantWithDuration.packedValue and DURATION_MINUTES_MASK, "packedValue")
-        assertEquals(0x7FF, instantWithDuration.packedValue, "packedValue should be 2047")
+        assertEquals(0x3ffffff, instantWithDuration.packedValue, "packedValue should be 67108863")
     }
 
     @Test
     fun `constructor with packedValue creates correct startEpochSeconds and durationMinutes`() {
         val instantWithDuration = InstantWithDuration(0x7ffL or DURATION_MINUTES_MASK)
         assertEquals(EPOCH_2020, instantWithDuration.startEpochSeconds)
-        assertEquals(InstantWithDuration.MAX_DURATION_MINUTES, instantWithDuration.durationMinutes)
+        assertEquals(MAX_DURATION_MINUTES, instantWithDuration.durationMinutes)
     }
 
     @ParameterizedTest
     @CsvSource(
-        "1630444800, 60, 1dhubhoc",
-        "1630448400, 120, 1dhypim0",
-        "1630448400, 121, 1dhypim1",
-        "1630448401, 120, 1dhypk6w",
-        "1630452000, 180, 1di33jjo"
+        "$EPOCH_2020, 0, 000000",
+        "$EPOCH_2020, 1, 000001",
+        "$EPOCH_2020, $DURATION_MINUTES_MASK, 13ydj3",
+        "${EPOCH_2020 + 60}, 0, 1ulajuo",
     )
     fun `roundtrip conversion maintains values`(
         startEpochSeconds: Long,
-        durationMinutes: UShort,
+        durationMinutes: UInt,
         expectedShortString: String
     ) {
         val instantWithDuration1 = InstantWithDuration.fromStartAndDuration(startEpochSeconds, durationMinutes)
@@ -64,20 +63,20 @@ class InstantWithDurationTest {
 
     @Test
     fun `endEpochSeconds - Year 2120, max duration`() {
-        val epoch2120 = 4733510400
+        val epoch2120 = 4733510400L
         val instantWithDuration = InstantWithDuration.fromStartAndDuration(
             epoch2120,
-            InstantWithDuration.MAX_DURATION_MINUTES
+            MAX_DURATION_MINUTES
         )
         assertEquals("2120-01-01T00:00:00Z", instantWithDuration.startFormatted, "startFormatted")
-        assertEquals("2120-01-02T10:07:00Z", instantWithDuration.endFormatted, "endFormatted")
+        assertEquals("2247-08-06T09:03:00Z", instantWithDuration.endFormatted, "endFormatted")
         assertEquals(
             epoch2120,
             instantWithDuration.startEpochSeconds,
             "startEpochSeconds"
         )
         assertEquals(
-            epoch2120 + InstantWithDuration.MAX_DURATION_MINUTES.toLong() * 60,
+            epoch2120 + MAX_DURATION_MINUTES.toLong() * 60,
             instantWithDuration.endEpochSeconds,
             "endEpochSeconds"
         )
@@ -88,11 +87,12 @@ class InstantWithDurationTest {
         "$EPOCH_2020, 0, '0m @ 2020-01-01T00:00:00Z'",
         "$EPOCH_2020, 1, '1m @ 2020-01-01T00:00:00Z'",
         "$EPOCH_2020, 2047, '2047m @ 2020-01-01T00:00:00Z'",
+        "$EPOCH_2020, $DURATION_MINUTES_MASK, '67108863m @ 2020-01-01T00:00:00Z'",
         "${EPOCH_2020 + 60}, 0, '0m @ 2020-01-01T00:01:00Z'",
     )
     fun `toShortString() for various inputs`(
         startEpochSeconds: Long,
-        durationMinutes: UShort,
+        durationMinutes: UInt,
         expected: String
     ) {
         assertEquals(
@@ -103,14 +103,16 @@ class InstantWithDurationTest {
 
     @ParameterizedTest
     @CsvSource(
-        "$EPOCH_2020, 0, '2020-01-01T00:00:00Z'",
-        "$EPOCH_2020, 1, '2020-01-01T00:01:00Z'",
+        "$EPOCH_2020,        0, '2020-01-01T00:00:00Z'",
+        "$EPOCH_2020,        1, '2020-01-01T00:01:00Z'",
         "${EPOCH_2020 + 60}, 0, '2020-01-01T00:01:00Z'",
-        "$EPOCH_2020, 2047, '2020-01-02T10:07:00Z'",
+        "$EPOCH_2020,     2047, '2020-01-02T10:07:00Z'",
+        "$EPOCH_2020,  $DURATION_MINUTES_MASK,          '2147-08-06T09:03:00Z'",
+        "${EPOCH_2020 + DURATION_MINUTES_MASK * 60}, 0, '2147-08-06T09:03:00Z'",
     )
     fun `'endFormatted' for various inputs`(
         startEpochSeconds: Long,
-        durationMinutes: UShort,
+        durationMinutes: UInt,
         expected: String
     ) {
         assertEquals(
